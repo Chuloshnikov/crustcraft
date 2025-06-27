9d53fdd19ff08424ddba8645ccd84c7f35733871
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2025-06-27 19:29:49 +0300

    done

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,10 @@
+import { ProfileContent } from "@/components/profile/ProfileContent";
+
+
+export default function ProfilePage() {
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-orange-50 to-red-50">
+        <ProfileContent />
+    </div>
+  )
+}
