0ef9b5ec0d6d544d0e37b6b0b266291c398ea991
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2025-07-01 21:03:15 +0300

    api added

--- src/app/api/user-info/route.ts
+++ src/app/api/user-info/route.ts
@@ -5,7 +5,46 @@ import { UserInfo } from "@/models/UserInfo";
 import { getServerSession } from "next-auth";
 
 
+export async function POST(req: Request) {
+    try {
+        await connectToDB();
+        const {
+            email,
+            firstName,
+            lastName,
+            avatarUrl,
+            dateOfBirth,
+            address,
+            phone,
+            admin
+        } = await req.json();
+        const session = await getServerSession(authOptions);
+        const userEntry = session?.user?.email;
+        if (!userEntry) {
+            return Response.json({ error: 'You need to be authenticated' }, { status: 404 });
+        }
 
+        const existingUser = await UserInfo.findOne({ email });
+            if (existingUser && userEntry === email) {
+                existingUser.set({
+                     email,
+                    firstName,
+                    lastName,
+                    avatarUrl,
+                    dateOfBirth,
+                    address,
+                    phone,
+                    admin
+                });
+                await existingUser.save();
+            } else {
+                return Response.json({ error: 'You do not have permission' }, { status: 400 });
+            }
+    } catch (error) {
+      console.error('Error:', error);
+      return Response.json({ error: 'Internal server error' }, { status: 500 });
+    }
+}
 
 
 export async function GET() {
@@ -13,7 +52,7 @@ export async function GET() {
     const session = await getServerSession(authOptions);
     const email = session?.user?.email;
     if (!email) {
-        return Response.json({});
+        return Response.json({ error: 'You need to be authenticated' }, { status: 400 });
     }
     return Response.json(
         await UserInfo.findOne({email})
