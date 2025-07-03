d09e7af86895e22fa093bcd76f98d3173c7d353c
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2025-07-03 20:49:39 +0300

    done

--- src/app/menu-items/new/page.tsx
+++ src/app/menu-items/new/page.tsx
@@ -1,7 +1,10 @@
+import { NewMenuItemForm } from '@/components/profile/menu-items/NewMenuItemForm'
 import React from 'react'
 
 export default function NewItem() {
   return (
-    <div>page</div>
+    <div className="min-h-screen bg-gradient-to-br from-orange-50 to-red-50">
+        <NewMenuItemForm />
+    </div>
   )
 }
