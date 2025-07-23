adb1ec41892e623d0ef0010eb18170bd4d4bf4d1
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2025-07-23 10:13:03 +0300

    done

--- src/app/api/checkout/route.ts
+++ src/app/api/checkout/route.ts
@@ -1,9 +1,9 @@
 import { authOptions } from "@/lib/auth/authOptions";
 import { connectToDB } from "@/lib/mongoose";
-import {IExtraPrice, MenuItem} from "@/models/MenuItem";
+import { MenuItem } from "@/models/MenuItem";
 import {Order} from "@/models/Order";
+
 import {getServerSession} from "next-auth";
-import { NextResponse } from "next/server";
 import Stripe from "stripe";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
@@ -22,68 +22,60 @@ export async function POST(req: Request) {
     paid: false,
   });
 
+  const stripeLineItems = [];
   for (const cartProduct of cartProducts) {
 
     const productInfo = await MenuItem.findById(cartProduct._id);
 
     let productPrice = productInfo.basePrice;
     if (cartProduct.size) {
-      const size = productInfo.sizes
-        .find(({size}: {size: IExtraPrice}) => size._id?.toString() === cartProduct.size._id.toString());
+      const size = productInfo.sizes 
+        .find((size: { _id: string; name: string; price: number }) => size._id.toString() === cartProduct.size._id.toString());
       productPrice += size.price;
     }
     if (cartProduct.extras?.length > 0) {
       for (const cartProductExtraThing of cartProduct.extras) {
-        const productExtras = productInfo.extraIngredientPrices;
+        const productExtras = productInfo.extraIngredients;
         const extraThingInfo = productExtras
-          .find(({extra}: {extra: IExtraPrice}) => extra._id?.toString() === cartProductExtraThing._id.toString());
+          .find((extra: { _id: string; name: string; price: number }) => extra._id.toString() === cartProductExtraThing._id.toString());
         productPrice += extraThingInfo.price;
       }
     }
 
     const productName = cartProduct.name;
 
-     const stripeLineItems: Stripe.Checkout.SessionCreateParams.LineItem[] = [
-    {
+    stripeLineItems.push({
       quantity: 1,
       price_data: {
-        currency: "USD",
+        currency: 'USD',
         product_data: {
           name: productName,
         },
         unit_amount: productPrice * 100,
       },
+    });
+  }
+
+  const stripeSession = await stripe.checkout.sessions.create({
+    line_items: stripeLineItems,
+    mode: 'payment',
+    customer_email: userEmail,
+    success_url: process.env.NEXTAUTH_URL + 'orders/' + orderDoc._id.toString() + '?clear-cart=1',
+    cancel_url: process.env.NEXTAUTH_URL + 'cart?canceled=1',
+    metadata: {orderId:orderDoc._id.toString()},
+    payment_intent_data: {
+      metadata:{orderId:orderDoc._id.toString()},
     },
-  ];
+    shipping_options: [
+      {
+        shipping_rate_data: {
+          display_name: 'Delivery fee',
+          type: 'fixed_amount',
+          fixed_amount: {amount: 500, currency: 'USD'},
+        },
+      }
+    ],
+  });
 
-        try {
-            const stripeSession = await stripe.checkout.sessions.create({
-              line_items: stripeLineItems,
-              mode: "payment",
-              customer_email: userEmail,
-              success_url: process.env.NEXTAUTH_URL + 'orders/' + orderDoc._id.toString() + '?clear-cart=1',
-              cancel_url: process.env.NEXTAUTH_URL + 'cart?canceled=1',
-              payment_intent_data: {
-                metadata: { orderId: orderDoc._id.toString() },
-              },
-              shipping_options: [
-                {
-                  shipping_rate_data: {
-                    display_name: 'Delivery fee',
-                    type: 'fixed_amount',
-                    fixed_amount: { amount: 500, currency: 'USD' },
-                  },
-                },
-              ],
-              metadata: { orderId: orderDoc._id.toString() },
-            });
-
-            return NextResponse.json({ url: stripeSession.url });
-        } catch (error: unknown) {
-            return NextResponse.json(
-            { error, message: "Could not create checkout session" },
-            { status: 500 }
-            );
-        }
-    }
+  return Response.json(stripeSession.url);
 }
