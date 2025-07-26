7cb455e027630457e1d635881be9447ab216ba22
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2025-07-26 22:27:55 +0300

    +

--- src/app/menu-items/edit/[id]/page.tsx
+++ src/app/menu-items/edit/[id]/page.tsx
@@ -5,25 +5,27 @@ import { Category } from "@/models/Category";
 import { MenuItem } from "@/models/MenuItem";
 import { redirect } from "next/navigation";
 
+interface PageProps {
+  params: {
+    id: string;
+  };
+}
 
-
-export default async function EditItem({ params }: { params: { id: string } }) {
+export default async function EditItem({ params }: PageProps) {
   const admin = await isAdmin();
   
   if (!admin) {
     redirect('/profile');
   }
     
-
   const { id } = params;
 
-  const categories = JSON.parse(JSON.stringify( await Category.find()));
-  const item = JSON.parse(JSON.stringify( await MenuItem.findById(id)));
-
+  const categories = JSON.parse(JSON.stringify(await Category.find()));
+  const item = JSON.parse(JSON.stringify(await MenuItem.findById(id)));
 
   return (
     <div className="min-h-screen bg-gradient-to-br from-orange-50 to-red-50">
-        <MenuItemForm
+      <MenuItemForm
         itemId={id}
         mode="edit"
         initialData={item}
@@ -31,5 +33,5 @@ export default async function EditItem({ params }: { params: { id: string } }) {
         onSuccessRedirect="/profile"
       />
     </div>
-  )
+  );
 }
