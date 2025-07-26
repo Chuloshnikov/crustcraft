c4b92782addfd9c525037a5a091d2e5f8b2d6a0a
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2025-07-26 12:38:56 +0300

    done

--- src/components/menu/MenuContainer.tsx
+++ src/components/menu/MenuContainer.tsx
@@ -1,11 +1,24 @@
+"use client"
+
 import MenuContent from "@/components/menu/MenuContent";
 import MenuHero from "@/components/menu/MenuHero";
+import { ICategory } from "@/models/Category";
+import { useEffect, useState } from "react";
 
-const MenuContainer = () => {
+const MenuContainer = ({categories}: {categories: ICategory}) => {
+  const [menuItems, setMenuItems] = useState(null);
+  const [selectedCategory, setSelectedCategory] = useState("all")
+  const [search, setSearch] = useState('');
+  
+
+  useEffect(() => {
+
+  },[])
+
   return (
     <>
-        <MenuHero />
-        <MenuContent />
+        <MenuHero search={search} setSearch={setSearch}/>
+        <MenuContent selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory} categories={categories}/>
     </>
   )
 }
--- src/components/menu/MenuHero.tsx
+++ src/components/menu/MenuHero.tsx
@@ -1,7 +1,7 @@
 import { Search } from "lucide-react"
 import { Input } from "@/components/ui/input"
 
-const MenuHero = () => {
+const MenuHero = ({ search, setSearch }: {search: string, setSearch: (value: string) => void }) => {
   return (
     <section className="py-16 bg-gradient-to-r from-orange-500 to-red-500 text-white">
       <div className="container mx-auto px-4 sm:px-6 lg:px-8">
@@ -16,6 +16,8 @@ const MenuHero = () => {
             <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 h-5 w-5 text-gray-400" />
             <Input
               type="text"
+              value={search}
+              onChange={e => setSearch(e.target.value)}
               placeholder="Search for your favorite dish..."
               className="pl-10 py-3 bg-white/90 border-0 text-gray-900 placeholder:text-gray-500"
             />
