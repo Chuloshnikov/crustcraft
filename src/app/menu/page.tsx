bf8ddaba093390f6937f71072aada49cef73e775
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2025-07-26 16:30:36 +0300

    done

--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -2,12 +2,10 @@ import MenuContainer from "@/components/menu/MenuContainer";
 import { Category } from "@/models/Category";
 
 export default async function MenuPage() {
-
-  const categories = JSON.parse(JSON.stringify( await Category.find()));
-
+  const categories = await Category.find().lean();
   return (
     <div className="min-h-screen bg-gray-50">
-        <MenuContainer categories={categories}/>
+      <MenuContainer categories={JSON.parse(JSON.stringify(categories))} />
     </div>
   )
 }
